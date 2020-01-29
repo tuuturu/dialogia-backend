a3efe93c4278d9c6ee40099bbf838fa536fb270f
Jan Erik Haavet <jan.haaavet@byr.oslo.kommune.no>
2020-01-29 13:38:20 +0100

    remove strict

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const express = require('express');
 const logger = require('morgan');
 const sendFeedback = require('./feedbackSender');
--- feedbackSender.js
+++ feedbackSender.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const request = require('request');
 
 const recipientEmailAddress = 'developerportal@oslo.kommune.no';
