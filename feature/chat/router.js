b4798d26529f8ce659dd36ca5216c7a70fec4fe9
Yngvar Kristiansen <yngkri@knowit.no>
2020-03-28 11:11:43 +0100

    POC chat

--- feature/chat/router.js
+++ feature/chat/router.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const WebSocket = require('ws');
 
 const router = express.Router()
 
@@ -7,3 +8,17 @@ router.get('/', async (req, res) => {
 })
 
 module.exports = router
+
+
+// CHAT
+console.log("Starting chat websocket server");
+
+const wss = new WebSocket.Server({ port: 8081 });
+
+wss.on('connection', function connection(ws) {
+	ws.on('message', function incoming(message) {
+		console.log('received: %s', message);
+	});
+
+	ws.send('something');
+});
