95754d58a694bfb14f74184d0c53a0cc10548b44
Julius Pedersen <deifyed@gmail.com>
2020-03-28 19:25:50 +0100

    ✅ Add some mock subjects

--- feature/common/subject_repository.js
+++ feature/common/subject_repository.js
@@ -1,6 +1,18 @@
 
 // subjectsDatabase = [ { value: 'covid-19', }, ]
-const subjectsDatabase = []
+const subjectsDatabase = [
+	{ value: 'covid-19', frequency: 78 },
+	{ value: 'coronavirus', frequency: 92 },
+	{ value: 'Life in isolation', frequency: 92 },
+	{ value: 'depression', frequency: 42 },
+	{ value: 'Hytteforbud', frequency: 54 },
+	{ value: 'hvordan overleve at alle i hustanden har brakkefeber', frequency: 67 },
+	{ value: 'jeg ble nettopp permitert, hva skal jeg gjøre', frequency: 48 },
+	{ value: 'Hack the Crisis!', frequency: 130 },
+	{ value: 'Norge after the crisis', frequency: 64 },
+	{ value: 'I want to get to know you!', frequency: 40 },
+	{ value: 'How we can help our farmers?', frequency: 55 }
+]
 
 function getAllSubjects() {
 	return [...subjectsDatabase]
