f1def9feeb5b046c938ee18f4ebc87d8926d4c50
Yngvar Kristiansen <yngkri@knowit.no>
2020-03-29 11:49:39 +0200

    👌Restructure websocket_server into more feature like parts

--- feature/chat/websocket_server.js
+++ feature/chat/websocket_server.js
@@ -1,7 +1,36 @@
 const WebSocket = require('ws');
 const { nanoid } = require('nanoid')
 
-console.log("_------------------------------- INITING CLIENTS")
+// --- FEATURE: Web socket server
+function start() {
+	const webSocketServer = new WebSocket.Server({ port: 8081 });
+
+	webSocketServer.on('connection', function connection(ws) {
+		ws.on('message', function incoming(message) {
+			console.log("- Received message", message);
+
+			const clientEvent = JSON.parse(message)
+
+			handleClientEvent(ws, clientEvent)
+		});
+	});
+}
+
+function handleClientEvent(websocket, clientEvent) {
+	if (clientEvent.type === "register")
+		handleClientEventRegister(websocket, clientEvent)
+	else if (clientEvent.type === "message")
+		broadcast(websocket, clientEvent)
+	else
+		console.error("No handler for type: " + clientEvent.type)
+}
+
+function handleClientEventRegister(websocket, clientEvent) {
+	registerClient(websocket, clientEvent)
+	sendServerEvent(websocket, )
+}
+
+// COMMON FOR FEATURES BELOW
 const clients = []
 
 class Client {
@@ -12,54 +41,25 @@ class Client {
 	}
 }
 
-function start() {
-	const webSocketServer = new WebSocket.Server({ port: 8081 });
-
-	webSocketServer.on('connection', function connection(ws) {
-		ws.on('message', function incoming(message) {
-			console.log("- Received message", message);
-
-			const clientEvent = JSON.parse(message)
-
-			handleClientEvent(webSocketServer, ws, clientEvent)
-		});
-	});
+function sendServerEvent(ws, serverEvent) {
+	console.log("Sending from server", serverEvent)
+	ws.send(JSON.stringify(serverEvent));
 }
 
-function createServerEvent(from, message) {
+// --- FEATURE: Register client
+function registerClient(websocket, clientEvent) {
+	clients.push(new Client(nanoid(), clientEvent.clientName, websocket))
+}
+
+// --- FEATURE: Broadcast
+function createServerMessageType(from, message) {
 	return {
+		type: "message",
 		from: from,
 		message: message
 	}
 }
 
-function sendServerEvent(ws, serverEvent) {
-
-	console.log("Sending from server", serverEvent)
-	ws.send(JSON.stringify(serverEvent));
-}
-
-function handleClientEvent(webSocketServer, websocket, clientEvent) {
-	if (clientEvent.type === "register")
-		registerClient(websocket, clientEvent);
-	else if (clientEvent.type === "message")
-		broadcast(websocket, clientEvent)
-	else
-		console.warn("No handler for type: " + clientEvent.type)
-}
-
-function registerClient(websocket, clientEvent) {
-	console.log("Registering client", clientEvent.clientName)
-	console.log("* Old number of clients: ", Object.keys(clients).length)
-	//console.log(websocket)
-	//clients[websocket] = new Client(nanoid(), clientEvent.clientName, websocket)
-	clients.push(new Client(nanoid(), clientEvent.clientName, websocket))
-	console.log("* New number of clients: ", Object.keys(clients).length)
-}
-
-function getClientByWebsocket(websocket) {
-	return clients.filter(client => client.websocket === websocket)[0]
-}
 
 function broadcast(sourceClientWebsocket, clientEvent) {
 	clients.forEach((client) => {
@@ -69,16 +69,19 @@ function broadcast(sourceClientWebsocket, clientEvent) {
 			console.log("broadcasting to ", client.name)
 			const sourceClient = getClientByWebsocket(sourceClientWebsocket)
 			console.log("sourceClient", sourceClient)
-			sendServerEvent(client.websocket, createServerEvent(sourceClient.name, clientEvent.message))
+			sendServerEvent(client.websocket, createServerMessageType(sourceClient.name, clientEvent.message))
 		} else {
 			console.log("NOT broadcasting to ", client.name)
 		}
 	})
-
-
 }
 
+// TODO: Innefective O(n^2) implementation when used from a for loop
+function getClientByWebsocket(websocket) {
+	return clients.filter(client => client.websocket === websocket)[0]
+}
 
+// --- OTHER
 module.exports = {
 	start
 }
